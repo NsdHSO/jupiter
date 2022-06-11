e475749f913935a6525fa840a07544f80959118c
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2022-06-11 11:33:40 +0000

    feat: added standard inpu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use std::io;
 
 fn main() {
+    println!("Please introduce some char: ");
+    let mut key = String::new();
+    io::stdin().read_line( key);
+    pointer_owner(&mut key);
     let e = Box::new(4);
     println!("{}",e);
-    println!("{}", calc_weight_on_mars(100.0));
+    println!("{}", 1000.0 *calc_weight_on_mars(10.9));
 }
 
 fn calc_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.71
+}
+
+fn pointer_owner(s: String){
+    println!("TEST : {}",s)
 }
